0f7c22af58def3e2017a15f5e05753747487ccfa
Rio Waller <riowaller@gmail.com>
2021-12-21 11:22:34 -0800

    displayOnPage function and files

---
+++ demo/display/app.js
@@ -0,0 +1,8 @@
+const showOnPage = function (text) {
+  let newParagraph = document.createElement("p")
+  newParagraph.innerHTML = text
+  let outputDiv = document.getElementById("output")
+  outputDiv.append(newParagraph)
+}
+
+showOnPage('Hello World')
